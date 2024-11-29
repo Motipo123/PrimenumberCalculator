d2d3abc5852f9a1e65e2a2ca9cafaec7bd785051
Motipo123 <167690665+Motipo123@users.noreply.github.com>
2024-11-29 20:38:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let number_matrix = document.querySelector("#numbermatrix");
+let matrix_IDK = 100;
+
+
+GenerateField();
+
+
+function GenerateField() {
+  let matrixHTML = "";
+  for (let index = 0; index < matrix_IDK; index++) {
+    matrixHTML += `<button id="Button-${index + 1}" class="Number-Button" onclick="onclickevent(${index + 1})">${index + 1}</button>`;
+  }
+  number_matrix.innerHTML = matrixHTML;
+}
+
+function getDivisibleNumbers(x) {
+  let result = [];
+  for (let i = x+1; i <= matrix_IDK; i++) {
+    if (i % x === 0) {
+      result.push(i);
+    }
+  }
+  return result;
+}
+
+function setButton(ButtonNumber, content) {
+  if (content == "strikethrough") {
+    document.querySelector(`#Button-${ButtonNumber}`).classList.add("strikethrough")
+  } else {
+    document.querySelector(`#Button-${ButtonNumber}`).innerHTML = content;
+  }
+}
+
+function onclickevent(number) {
+  //console.log("Clicked "+number);
+  Divisible = getDivisibleNumbers(number);
+  Divisible.forEach(element => {
+    setButton(element, "strikethrough");
+  });
+
+};
+
+document.querySelector("#Board-Size-Input").addEventListener("keydown", (event) => {
+  if (event.key === 'Enter') {
+      matrix_IDK = document.querySelector("#Board-Size-Input").value;
+      GenerateField();
+  }
+});
+
+function ResetBoard() {
+  const ButtonArray = document.querySelectorAll(".Number-Button");
+  ButtonArray.forEach((button) => {
+    button.classList.remove("strikethrough");
+  });
+};
